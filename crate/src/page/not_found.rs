d59ba52b43a0432ef9cc13aa346a48a480f0645e
direstrepo24 <direstrepobr@gmail.com>
2021-06-01 19:25:47 -0500

    Initialize realworld_alternative_gloo

---
+++ crate/src/page/not_found.rs
@@ -0,0 +1,23 @@
+use super::ViewPage;
+use crate::entity::Image;
+use seed::prelude::*;
+
+// ------ ------
+//     View
+// ------ ------
+
+pub fn view<'a, Ms: Clone>() -> ViewPage<'a, Ms> {
+    ViewPage::new("Page Not Found", view_content())
+}
+
+// ====== PRIVATE ======
+
+fn view_content<Ms: Clone>() -> Node<Ms> {
+    main![
+        id!("content"),
+        class!["container"],
+        attrs! {At::TabIndex => -1},
+        h1!["Not Found"],
+        div![class!["row"], img![attrs! {At::Src => Image::error().url()}]]
+    ]
+}
